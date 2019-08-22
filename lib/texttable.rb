144ecd3094e773eb5c8d53b89b1471d510afb5f7
Steve Shreeve <steve.shreeve@gmail.com>
2019-08-22 04:16:34 -0600

    Add csv, tsv, psv, and sql methods

--- lib/texttable.rb
+++ lib/texttable.rb
@@ -28,4 +28,28 @@ class TextTable
     puts line, "#{seen} rows displayed", ""
     self
   end
+
+  def csv(sep=',', encoding: nil)
+    require 'csv'
+    csv = {}
+    csv[:encoding] = encoding + ":UTF-8" if encoding
+    csv[:col_sep ] = sep
+    csv = CSV.new($stdout, csv)
+    csv << cols.keys
+    rows.each {|vals| csv << vals}
+  end
+  def tsv; csv("\t"); end
+  def psv; csv("|" ); end
+
+  def sql(table='table', quote: false)
+    q = quote ? '`' : ''
+    flip = cols.invert
+    rows.each do |vals|
+      list = vals.each_with_index.inject([]) do |list, (item, i)|
+        list << "#{q}#{flip[i]}#{q}='#{item.gsub("'","''")}'" if item =~ /\S/
+        list
+      end
+      puts "insert into #{q}#{table}#{q} set #{list * ', '};" if !list.empty?
+    end
+  end
 end
